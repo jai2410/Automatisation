ba6a8529a795ff54ffa360ecbf3f6dae7630bc0d
jay <jaysoni2410@gmail.com>
2023-05-29 18:35:28 +0200

    Update

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from time import strftime
-from action import action_script
+import sys
+from action import ActionScript
 from automated_person import AutomatedPerson
+import json
+
+# Define all your users
+users = {
+    "1": AutomatedPerson("Phillip", 1, 2),
+    "2": AutomatedPerson("Jane", 2, 2),
+    # Add more users as required...
+}
 
 if __name__ == "__main__":
-    jan = AutomatedPerson("Phillip", 1, 1)
-    new_script = action_script(jan)
-    new_script.perform()
+    # Load the action script
+    with open('config.json', 'r') as file:
+        actions = json.load(file)
+
+    # Execute the actions
+    for action in actions:
+        user_id = action["id"]
+        user = users[user_id]
+        script = ActionScript(user, action)
+        script.perform()
+
